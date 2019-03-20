891d8aa738fe2a3b7f01235db22b00de3fbb3b01
jacobgreen4477 <jacobgreen4477@gmail.com>
2019-03-20 23:50:57 +0900

    add

--- BTCI100_OP.R
+++ BTCI100_OP.R
@@ -6,8 +6,8 @@ library(data.table)
 library(plotly)
 
 # read data 
-data_w_yhat = fread("./BITCOIN/output/backtest_20190222.csv")
-data_w_y = fread("./BITCOIN/output/realdata_20190320.csv")
+data_w_yhat = fread("C:/Users/user/Documents/BITCOIN/output/backtest_20190222.csv")
+data_w_y = fread("C:/Users/user/Documents/BITCOIN/output/realdata_20190320.csv")
 
 # date 
 data_w_yhat[,ymd:=as.numeric(gsub("-","",substr(V1,1,10)))]
@@ -24,12 +24,15 @@ data_w_y = data_w_y[ymd>=20190222 & ymd<20190320,list(date,Y=close)]
 # merge
 data = merge(data_w_y,data_w_yhat,by="date",all.y=T)
 data[,date:=substr(date,1,10)]
+head(data)
 
 # plot
-ply = plot_ly(data, x = ~date) %>%
+plot_ly(data, x = ~date) %>%
   add_trace(y = ~Y, type = 'scatter', mode = 'lines', line = list(color = 'blue', width = 1), name="실제값") %>%
   add_trace(y = ~Yhat, type = 'scatter', mode = 'lines', line = list(color = 'red', width = 1, dash='dot'), name="예측값") %>% 
-  layout(title="2019년 3월 비트코인 예측결과",xaxis=list(fixedrange=TRUE, title="date"),yaxis=list(fixedrange=TRUE, title="price")) %>% 
-  config(displayModeBar = F)
+  layout(title="2019년 3월 비트코인 예측결과",xaxis=list(fixedrange=F, title="date"),yaxis=list(fixedrange=F, title="price")) 
 
-htmlwidgets::saveWidget(as_widget(ply), "./BTCI100_OP_201903.html")
+# htmlwidgets::saveWidget(ply, "./BTCI100_OP_201903.html")
+
+
+
